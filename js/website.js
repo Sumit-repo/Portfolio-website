602510eb8b4b08db3b4b32fe26310b1755163c86
Sumit <1020sumit@gmail.com>
2022-06-06 05:05:48 +0530

    Portfolio Done

--- js/website.js
+++ js/website.js
@@ -43,8 +43,8 @@ function toggleSkills() {
     }
 }
 
-skillsHeader.forEach((eL) => {
-    eL.addEventListener('click', toggleSkills)
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
 })
 
 /* =========services========= */
@@ -70,6 +70,7 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
+/* =========Portfolio========= */
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
@@ -82,3 +83,64 @@ let swiper = new Swiper(".portfolio__container", {
         clickable: true,
     },
 });
+
+const sections = document.querySelectorAll('section[id]')
+
+/* =========Scroll========= */
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.content a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.content a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* =========Change background header========= */
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* =========Scroll up========= */
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+// Previously selected topic(if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+// We validate if the user previously choseatopic
+if (selectedTheme) {
+    // If the validation is fulfilled,we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark/icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
